fa395451085a87e7022715b1bcce5615b0fd94ee
Cody Michaud <cody.r.michaud@gmail.com>
2021-03-17 23:17:40 -0700

    Finished view all and addemployee function. starting on viewing employees by department

--- server.js
+++ server.js
@@ -103,8 +103,8 @@ function addEmployee() {
         message: 'What is the Managers name?',
         choices: getManager()
     }]).then((answers) => {
-        const rolesId = selectRole().indexOf(answers.role) + 1;
-        const managerId = selectManager().indexOf(answers.choice) + 1;
+        const rolesId = getRole().indexOf(answers.role) + 1;
+        const managerId = getManager().indexOf(answers.choice) + 1;
         connection.query('INSERT INTO employee SET ?', {
             first_name: answers.firstName,
             last_name: answers.lastName,
@@ -114,6 +114,14 @@ function addEmployee() {
             if (error) throw error;
             console.table(answers);
             startPrompt();
-        })
+        });
+    });
+};
+
+function viewAll() {
+    connection.query('SELECT employee.first_name, employee.last_name, roles.title, roles.salary, department.name, CONCAT(e.first_name, " ", e.last_name) AS manager FROM employee INNER JOIN roles ON roles.id = employee.role_id INNER JOIN department ON department.id = roles.department_id LEFT JOIN employee e ON employee.manager_id = e.id;', (error, res) => {
+        if (error) throw error;
+        console.table(res);
+        startPrompt();
     })
 }
