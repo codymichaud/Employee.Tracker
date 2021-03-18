6f821dc4759d7ce393c51aa0f4493ffa5be7c7c8
Cody Michaud <cody.r.michaud@gmail.com>
2021-03-17 22:22:38 -0700

    Created connection with mysql and started the connection

--- server.js
+++ server.js
@@ -2,4 +2,19 @@ const mysql = require('mysql2/promise');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
+//Creating Database connection
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Broccoli2016!',
+    database: 'employee_trackDB'
+});
+
+connection.connect((error) => {
+    if (error) throw error;
+    startPrompt();
+});
+
 
