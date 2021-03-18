d937a5eec53b1b5291f49e755a46f7574c3c455a
Cody Michaud <cody.r.michaud@gmail.com>
2021-03-17 22:44:55 -0700

    created start prompt function

--- server.js
+++ server.js
@@ -17,4 +17,50 @@ connection.connect((error) => {
     startPrompt();
 });
 
+function startPrompt() {
+    inquirer.prompt([{
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'choice',
+        choices: [
+            'View All Employees',
+            'View All Employees By Department',
+            'View All Employees By Manager',
+            'Add Employee',
+            'Update Employee'
+        ]
+    }]).then((answers) => {
+        switch (answers.choice) {
+            case 'View All Employees':
+                viewAll();
+                break;
 
+            case 'View All Employees By Department':
+                viewDepart();
+                break;
+
+            case 'View All Employees By Manager':
+                viewManager();
+                break;
+
+            case 'Add Employee':
+                addEmployee();
+                break;
+
+            case 'Update Employee':
+                updateEmployee();
+                break;
+
+            default:
+                break;
+        }
+    });
+};
+
+const roleArray = [];
+function getRole() {
+    connection.query('SELECT * FROM roles', (error, res) => {
+        if (error) throw error;
+        for ()
+    })
+}
